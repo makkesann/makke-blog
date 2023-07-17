1689653f144cfab760faa401140b90da019a5e74
makkesann <makkesann@gmail.com>
2023-07-17 21:57:42 +0900

    untracked files on master: ec8abb9 ホーム画面へんこう

---
+++ src/component/header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="flex justify-between mx-12">
+        <div>
+          わっしょい
+        </div>
+        <div>
+          わっしょい
+        </div>
+      </div>
+    </header>
+  )
+}
